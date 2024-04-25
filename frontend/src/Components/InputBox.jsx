83073bb83d1c80cb4ba369b047fa0470e92e0773
Aryan Pachori <aryanpachori03@gmail.com>
2024-04-26 05:23:14 +0530

    routes

--- frontend/src/Components/InputBox.jsx
+++ frontend/src/Components/InputBox.jsx
@@ -1,9 +1,10 @@
 /* eslint-disable react/prop-types */
-export function InputBox({ placeholder, label }) {
+export function InputBox({ placeholder, label, onChange }) {
   return (
     <div>
       <div className="text-sm font-medium text-left py-2">{label}</div>
       <input
+        onChange={onChange}
         placeholder={placeholder}
         className=" w-full px-2 py-1 border rounded-slate-200"
       />
--- frontend/src/pages/SignUp.jsx
+++ frontend/src/pages/SignUp.jsx
@@ -1,26 +1,67 @@
 import { Heading } from "../Components/Heading";
 import { SubHeading } from "../Components/SubHeading";
-import {InputBox} from "../Components/InputBox"
-import {Button} from "../Components/Button"
-import {BottomWarning} from "../Components/BottomWarning"
-
+import { InputBox } from "../Components/InputBox";
+import { Button } from "../Components/Button";
+import { BottomWarning } from "../Components/BottomWarning";
+import { useState } from "react";
+import axios from 'axios';
 export function SignUp() {
-  return(
-    <div className=" bg-slate-300 h-screen flex justify-center">
-    <div className="flex flex-col justify-center">
-      <div className="rounded-lg bg-white w-80 text-center p-2 h-max px-4">
-      <Heading label={"Sign Up"}/>
-      <SubHeading label={"Enter your information to create an account :)"}/>
-       <InputBox label={"First Name"} placeholder={"John"}/>
-       <InputBox label={"Last Name"} placeholder={"Doe"}/>
-       <InputBox label={"Email"} placeholder={"example@gmail.com"}/>
-       <InputBox label={"Password"} placeholder={"*****"}/>
-       <Button label={"SIGN UP"}/>
-       <BottomWarning label={"Already have an account ?"} buttonText={"Sign In"} />
-      </div>
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [firstName, setFirstName] = useState("");
+  const [lastname, setLastName] = useState("");
 
+  return (
+    <div className=" bg-slate-300 h-screen flex justify-center">
+      <div className="flex flex-col justify-center">
+        <div className="rounded-lg bg-white w-80 text-center p-2 h-max px-4">
+          <Heading label={"Sign Up"} />
+          <SubHeading
+            label={"Enter your information to create an account :)"}
+          />
+          <InputBox
+            onChange={(e) => {
+              setFirstName(e.target.value);
+            }}
+            label={"First Name"}
+            placeholder={"John"}
+          />
+          <InputBox
+            onChange={(e) => {
+              setLastName(e.target.value);
+            }}
+            label={"Last Name"}
+            placeholder={"Doe"}
+          />
+          <InputBox
+            onChange={(e) => {
+              setUsername(e.target.value);
+            }}
+            label={"Email"}
+            placeholder={"example@gmail.com"}
+          />
+          <InputBox
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
+            label={"Password"}
+            placeholder={"*****"}
+          />
+          <Button onClick={()=>{
+            axios.post("http://localhost:3000/api/v1/user/signup",{
+              username,
+              password,
+              firstName,
+              lastname
+            })
+          }}
+          label={"SIGN UP"} />
+          <BottomWarning
+            label={"Already have an account ?"}
+            buttonText={"Sign In"}
+          />
+        </div>
+      </div>
     </div>
-   
-    </div>
-  )
+  );
 }
